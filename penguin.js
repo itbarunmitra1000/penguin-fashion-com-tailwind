b44b2dfc01ba14f331832039b91e541b11c6646d
Barun Mitra <itbarunmitra1000@gmail.com>
2021-08-17 20:42:23 +0600

    add js file

---
+++ penguin.js
@@ -0,0 +1,6 @@
+const buyNow = document.getElementById('buy-now');
+buyNow.addEventListener('click',function(){
+    // console.log('clicked');
+    const headerTextChange = document.getElementById('header-text-change');
+    headerTextChange.innerText = 'I AM PENGUIN';
+})
